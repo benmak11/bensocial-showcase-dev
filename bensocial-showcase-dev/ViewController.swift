1d46ad5c25c6039a5b4a0111145be0b4908199a1
Ben Makusha <btmakusha@gmail.com>
2016-04-23 19:16:59 -0400

    Firebase login with Facebook and email done. User's can now enter login details and get an account created automatically.

--- bensocial-showcase-dev/ViewController.swift
+++ bensocial-showcase-dev/ViewController.swift
@@ -11,15 +11,22 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if NSUserDefaults.standardUserDefaults().valueForKey(KEY_UID) != nil {
+            self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+        }
     }
     
     @IBAction func fbBtnPressed(sender: UIButton!){
@@ -40,7 +47,7 @@ class ViewController: UIViewController {
                     } else {
                         print("Login In! \(authData)")
                         NSUserDefaults.standardUserDefaults().setValue(authData.uid, forKey: KEY_UID)
-                        self.performSegueWithIdentifier("loggedIn", sender: nil)
+                        self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
                     }
                 
                 })
@@ -48,6 +55,52 @@ class ViewController: UIViewController {
             }
         }
         
+    }
+    
+    @IBAction func attemptLogin(sender: UIButton){
+        
+        if let email = emailField.text where email != "", let pwd = passwordField.text where pwd != "" {
+            
+            DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: { error, authData in
+                
+                if error != nil {
+                    
+                    print(error)
+                    
+                    if(error.code == STATUS_ACCOUNT_NONEXIST) {
+                        DataService.ds.REF_BASE.createUser(email, password: pwd, withValueCompletionBlock: { error, result in
+                        
+                            if error != nil {
+                                self.showErrorAlert("Could not create account", msg: "Problem creating account. Try something else.")
+                            } else{
+                                //Now creating account
+                                NSUserDefaults.standardUserDefaults().setValue(result[KEY_UID], forKey: KEY_UID)
+                                
+                                DataService.ds.REF_BASE.authUser(email, password: pwd, withCompletionBlock: nil)
+                                self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                            }
+                            
+                        })
+                    } else{
+                        self.showErrorAlert("Could not login.", msg: "Please check your username and password")
+                    }
+                } else {
+                    
+                    self.performSegueWithIdentifier(SEGUE_LOGGED_IN, sender: nil)
+                }
+            
+            })
+            
+        } else {
+            showErrorAlert("Email and Password Required!", msg: "You must enter an email and a password")
+        }
+    }
+    
+    func showErrorAlert(title: String, msg: String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .Alert)
+        let action = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
     }
 
 }

06d534f5672662f197942f50357a38d6a6b88d54
Ben Makusha <btmakusha@gmail.com>
2016-04-23 11:20:22 -0400

    Established the login screen - Built the splash landing page   - Added the Facebook login images, button features   - Created custom classes for making shadowColor backgrounds, border radius, etc...

---
+++ bensocial-showcase-dev/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  bensocial-showcase-dev
+//
+//  Created by Baynham Makusha on 4/23/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0    //Greyish shadow color
+
---
+++ bensocial-showcase-dev/MaterialView.swift
@@ -0,0 +1,22 @@
+//
+//  MaterialView.swift
+//  bensocial-showcase-dev
+//
+//  Created by Baynham Makusha on 4/23/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+    
+    override func awakeFromNib() {
+        //Customizing the views
+        layer.cornerRadius = 2.0
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).CGColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSizeMake(0.0, 2.0)
+    }
+
+}

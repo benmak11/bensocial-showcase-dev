79fcb2ac27f4af2736d6d95de178c7786467deb7
Ben Makusha <btmakusha@gmail.com>
2016-04-23 10:15:05 -0400

    Initial Commit

---
+++ bensocial-showcase-dev/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bensocial-showcase-dev
+//
+//  Created by Baynham Makusha on 4/23/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

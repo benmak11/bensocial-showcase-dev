32327507fc11fd8dc9210a43220131040a59bbe9
Ben Makusha <btmakusha@gmail.com>
2016-04-24 19:48:46 -0400

    Talking to Firebase and downloading and parsing data and saved in the model

--- bensocial-showcase-dev/FeedVC.swift
+++ bensocial-showcase-dev/FeedVC.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,11 +20,30 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.delegate = self
         tableView.dataSource = self
         
-        //Downloading the data for updating the UI
+        //Downloading the data from Firebase
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
         
+            // Test printing data
             print(snapshot.value)
-        
+            
+            
+            self.posts = []    // Clearing the post data
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    
+                    //Parsing the data out of the Dictionary and into the post object
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
+            self.tableView.reloadData()
         })
     }
     
@@ -31,10 +52,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print(post.postDescription)
         
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }

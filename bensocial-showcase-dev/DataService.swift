82145d28d633e55bf34886730e6d625de52b85b8
Ben Makusha <btmakusha@gmail.com>
2016-04-24 18:24:32 -0400

    Implemented Firebase constants and Dataservice variables for users, posts, etc...

--- bensocial-showcase-dev/DataService.swift
+++ bensocial-showcase-dev/DataService.swift
@@ -9,11 +9,15 @@
 import Foundation
 import Firebase
 
+let URL_BASE = "https://bensocial-showcase.firebaseio.com"
+
 class DataService {
     
     static let ds = DataService()
     
-    private var _REF_BASE = Firebase(url: "https://bensocial-showcase.firebaseio.com")
+    private var _REF_BASE = Firebase(url: "\(URL_BASE)")
+    private var _REF_POSTS = Firebase(url: "\(URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(URL_BASE)/users")
     
     var REF_BASE: Firebase {
         return _REF_BASE
